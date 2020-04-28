c4622616f1f50ee76c9142e22cb7310e87134241
Rollin Francois <rfrancois21@hotmail.com>
2020-04-28 19:01:07 -0400

    Modified functionality for Obese range

--- BMI Calculator/Models/CalculatorLogic.swift
+++ BMI Calculator/Models/CalculatorLogic.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 
 struct CalculatorLogic {
@@ -19,13 +20,15 @@ struct CalculatorLogic {
         
         if bmiValue < 18.5 {
             bmi = BMI(value: bmiValue, advice: "You are underweight!", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
-            print("You are underweight")
+            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
         } else if bmiValue < 24.5 {
             bmi = BMI(value: bmiValue, advice: "You are quite healthy!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
-            print("Normal BMI range")
+        } else if bmiValue < 40 {
+            bmi = BMI(value: bmiValue, advice: "You are overweight", color: #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1))
+            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
         } else {
-            bmi = BMI(value: bmiValue, advice: "You are overweight", color: #colorLiteral(red: 1, green: 0.9028479042, blue: 0.3672789952, alpha: 1))
-            print("You are overweigh")
+            bmi = BMI(value: bmiValue, advice: "You are OBESE FR!!", color: #colorLiteral(red: 1, green: 0.1133150124, blue: 0.04508389556, alpha: 1))
+            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
         }
         
     }
